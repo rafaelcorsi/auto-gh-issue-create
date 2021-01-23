c69f32fe5ede03057531a7959095792c75d023fe
Rafael Corsi <corsiferrao@gmail.com>
2021-01-23 17:37:27 -0300

    agora valida para não criar o issue novamente, trabalhando no milestone

--- main.py
+++ main.py
@@ -6,8 +6,12 @@
 
 import argparse
 import yaml
+import json
 import os
 import git
+import pipes
+import tempfile
+import subprocess
 
 # https://stackoverflow.com/questions/19687394/python-script-to-determine-if-a-directory-is-a-git-repository
 def is_git_repo(path):
@@ -18,8 +22,60 @@ def is_git_repo(path):
         return False
 
 
+# TODO: precisa criar o alias antes, tirar isso
+# gh alias set createMilestone 'api --method POST repos/:owner/:repo/milestones --input'
+def ghMilestoneCreate(milestone):
+    p = pipes.Template()
+    p.append('gh createMilestone')
+    p.debug(True)
+
+    # Pass some text through the pipeline,
+    # saving the output to a temporary file.
+    t = tempfile.NamedTemporaryFile(mode='r')
+    f = p.open(t.name, 'w')
+    try:
+        f.write(j)
+    finally:
+        f.close()
+
+    # Rewind and read the text written
+    # to the temporary file
+    t.seek(0)
+    print(t.read())
+    t.close()
+
+    j = json.dumps(milestone)
+    command = 'gh createMilestone \'{}\''.format(j)
+    print(command)
+    os.system(command)
+
+
+def ghMilestoneCreateBulk(milestone, reposPath):
+    os.chdir(reposPath)
+    for repo in os.listdir():
+        if is_git_repo(repo):
+            os.chdir(repo)
+            print('repo: {}'.format(repo))
+            for m in milestone:
+                ghMilestoneCreate(milestone[m])
+        os.chdir('..')
+    os.chdir('..')
+
+
+def ghIssueList():
+    command = 'issue list -s all'
+    print()
+    out = subprocess.check_output('gh {}'.format(command), shell=True).decode('utf-8')
+    return(out)
+
+
+def ghIssueExist(issueList, issue):
+    if issueList.find(issue['Title']) > 0:
+        return True
+    return False
+
+
 def ghIssueCreeate(issue):
-#    import pdb; pdb.set_trace()
     title = '\'{}\''.format(issue['Title'])
     body = '\'{}\''.format(issue['Body'])
     command = 'gh issue create -t {} -b {}'.format(title,body)
@@ -28,16 +84,22 @@ def ghIssueCreeate(issue):
     os.system(command)
 
 
-def ghissueCreateBulk(issues, reposPath):
+def ghIssueCreateBulk(issues, reposPath):
     os.chdir(reposPath)
     for repo in os.listdir():
         if is_git_repo(repo):
             os.chdir(repo)
             print('repo: {}'.format(repo))
+            issueList = ghIssueList()
             for k, v in issues.items():
-                print('   - criando issue: {}'.format(k))
-                ghIssueCreeate(v)
+                if ghIssueExist(issueList, v) is False:
+                    print('   - criando issue: {}'.format(v['Title']))
+                    ghIssueCreeate(v)
+                else:
+                    print('   - issue já existia: {}'.format(v['Title']))
             os.chdir('..')
+    os.chdir('..')
+
 
 def initRepo(repoList, reposPath):
     for repo in repoList:
@@ -51,16 +113,19 @@ def initRepo(repoList, reposPath):
 if __name__ == '__main__':
     argparse.ArgumentParser()
     parser = argparse.ArgumentParser(prog='Automatic issue create on github - CLI')
-    parser.add_argument('--issues', default=None,  type=str, help='Issues (yml)')
-    parser.add_argument('--repos', default=None,  type=str, help='Repositorios (yml)')
+    parser.add_argument('--config', default=None,  type=str, help='Issues (yml)')
+    parser.add_argument('--issues', default=False, action='store_true', help='Cria os issues')
+    parser.add_argument('--milestone', default=False, action='store_true', help='Cria os milestones')
 
+    parser.add_argument('--repos', default=None,  type=str, help='Repositorios (yml)')
     parser.add_argument('--repos_dir', default='repos',  type=str, help='Pasta com os repositorios')
     parser.add_argument('--repos_update', default=False, action='store_true', help='Atualiza/Clona repositorios')
 
     args = parser.parse_args()
 
-    with open(args.issues, 'r') as file:
-        issues = yaml.load(file, Loader=yaml.FullLoader)
+    if(args.config is not None):
+        with open(args.config, 'r') as file:
+            config = yaml.load(file, Loader=yaml.FullLoader)
 
     if(args.repos is not None):
         with open(args.repos, 'r') as file:
@@ -70,4 +135,10 @@ if __name__ == '__main__':
         print('Atualizando/Clonando repositorios')
         initRepo(repos, args.repos_dir)
 
-    ghissueCreateBulk(issues, args.repos_dir)
+    if args.milestone:
+        print('Criando milestones')
+        ghMilestoneCreateBulk(config['milestone'], args.repos_dir)
+
+    if args.issues:
+        print('Criando issues')
+        ghIssueCreateBulk(config['issues'], args.repos_dir)
